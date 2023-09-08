22b4bc82980d8454c17533500e8a07e5abd46c9b
Lyxnx <a.julnes@gmail.com>
2023-09-08 11:21:38 +0100

    Configure publishing in plugin common

--- buildSrc/src/main/kotlin/plugin-commons.gradle.kts
+++ buildSrc/src/main/kotlin/plugin-commons.gradle.kts
@@ -1,25 +1,49 @@
-import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+import com.vanniktech.maven.publish.SonatypeHost
+import java.util.Properties
 
 plugins {
-    id("kotlin")
-    // TODO apply publishing plugin
+    id("kotlin-commons")
+    id("com.vanniktech.maven.publish")
 }
 
-kotlin {
-    explicitApi = ExplicitApiMode.Strict
+group = "io.github.lyxnx.gradle"
+version = getRootProperties()["kradle.version"].toString()
+description = "Various gradle plugins for regular JVM and Android"
+
+mavenPublishing {
+    coordinates(project.group.toString(), project.name, project.version.toString())
+
+    publishToMavenCentral(SonatypeHost.Companion.S01, true)
+    signAllPublications()
+
+    pom {
+        name.set("Kradle")
+        description.set(project.description)
+        inceptionYear.set("2023")
+        url.set("https://github.com/Lyxnx/kradle")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("http://www.opensource.org/licenses/mit-license.php")
+            }
+        }
+        developers {
+            developer {
+                id.set("Lyxnx")
+                name.set("Lyxnx")
+                url.set("https://github.com/Lyxnx")
+            }
+        }
+        scm {
+            url.set("https://github.com/Lyxnx/kradle")
+            connection.set("https://github.com/Lyxnx/kradle.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Lyxnx/kradle.git")
+        }
+    }
 }
 
-// TODO use kotlin config plugin once 1st version is published to configure this sort of stuff
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks.withType(KotlinCompile::class).configureEach {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_11)
+fun getRootProperties(): Properties {
+    return file("../gradle.properties").bufferedReader().use {
+        Properties().apply { load(it) }
     }
 }
